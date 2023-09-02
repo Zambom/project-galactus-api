ecc34d9b7aa1d12c0b71e24b3b0f98993e42ac48
Zambom <erikzambom@hotmail.com>
2023-09-02 14:54:17 -0300

    feat: adding crud operations endpoints to galaxies

--- src/router/galaxy.js
+++ src/router/galaxy.js
@@ -15,4 +15,53 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.post('/', async (req, res) => {
+    try {
+        const { name, description } = req.body
+
+        const instance = await controller.Create({ name, description })
+
+        res.status(201).send({ data: instance })
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const instance = await controller.Get(id)
+
+        res.status(200).send({ data: instance })
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const data = req.body
+
+        const instance = await controller.Update(id, data)
+
+        res.status(200).send({ data: instance })
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+
+        await controller.Delete(id)
+
+        res.status(204).send()
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
 module.exports = router
